76ad7ad537c07536d15e408f1da1eaa89d897ee2
MatreXiXesS <zurskij@gmail.com>
2024-09-30 18:28:38 +0300

    remake db connection

--- database/Harbour.php
+++ database/Harbour.php
@@ -1,11 +1,15 @@
 <?php
+    namespace Database;
+
+    use mysqli;
+
     class Harbour {
         public readonly string $databaseHost;
         public readonly string $databaseUser;
         public readonly string $databasePassword;
         public readonly string $databaseName;
 
-        public function databaseConnect($dbHost, $dbUser, $dbPass, $dbName) {
+        public function databaseConnect(string $dbHost, string $dbUser, string $dbPass, string $dbName) {
             return new mysqli(
             $this -> databaseHost = $dbHost,
             $this -> databaseUser = $dbUser,
