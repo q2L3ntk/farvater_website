58d10a9014fedcf28446ca326bd9dd13dbff3585
MatreXiXesS <zurskij@gmail.com>
2024-09-30 20:31:05 +0300

    services 01

--- resources/views/services.php
+++ resources/views/services.php
@@ -8,6 +8,9 @@
     <link rel="stylesheet" href="/resources/css/style/services.css">
     <link rel="stylesheet" href="/resources/css/media/services-media.css">
 
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="/resources/js/animate/services-animate.js"></script>
+    <script src="/resources/js/path/services-path.js"></script>
 </head>
 <body>
     
